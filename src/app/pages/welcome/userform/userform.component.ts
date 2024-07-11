f42a9d3f6a75d70bb9e91e7e8f7787aa9e8a9bc3
ggrents <chef_grents@mail.ru>
2024-07-11 21:31:42 +0500

    add welcome page layout

---
+++ src/app/pages/welcome/userform/userform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-userform',
+  standalone: true,
+  imports: [],
+  templateUrl: './userform.component.html',
+  styleUrl: './userform.component.scss'
+})
+export class UserformComponent {
+
+}
