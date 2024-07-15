8aedfd6d39928fe9043c724693be9faaee0ab016
ggrents <chef_grents@mail.ru>
2024-07-15 22:19:04 +0500

    complete sidebar component

--- src/app/pages/shared/sidebar/sidebar.component.ts
+++ src/app/pages/shared/sidebar/sidebar.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+
+  selectedItem: string = ''; 
+
+  selectItem(item: string) {
+    this.selectedItem = item; 
+  }
 
 }
--- src/app/pages/trainer/trainer.component.ts
+++ src/app/pages/trainer/trainer.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { TextComponent } from "./text/text.component";
 import { InputComponent } from "./input/input.component";
 import { KeyboardComponent } from "./keyboard/keyboard.component";
+import { SidebarComponent } from "../shared/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-trainer',
   standalone: true,
-  imports: [TextComponent, InputComponent, KeyboardComponent],
+  imports: [TextComponent, InputComponent, KeyboardComponent, SidebarComponent],
   templateUrl: './trainer.component.html',
   styleUrl: './trainer.component.scss'
 })
