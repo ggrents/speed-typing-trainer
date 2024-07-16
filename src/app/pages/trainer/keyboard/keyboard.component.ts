f11321a553b4eb4dc950c5725c40b8f46135708c
ggrents <chef_grents@mail.ru>
2024-07-16 21:45:51 +0500

    html css keyboard

--- src/app/pages/trainer/keyboard/keyboard.component.ts
+++ src/app/pages/trainer/keyboard/keyboard.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface IKey {
+  pushed: boolean;
+  mark: string;
+}
+
 @Component({
   selector: 'app-keyboard',
   standalone: true,
@@ -7,6 +12,43 @@ import { Component } from '@angular/core';
   templateUrl: './keyboard.component.html',
   styleUrl: './keyboard.component.scss'
 })
-export class KeyboardComponent {
 
+export class KeyboardComponent {
+  keyboardButtons: IKey[] = [
+    { pushed: false, mark: "й" },
+    { pushed: false, mark: "ц" },
+    { pushed: false, mark: "у" },
+    { pushed: false, mark: "к" },
+    { pushed: false, mark: "е" },
+    { pushed: false, mark: "н" },
+    { pushed: false, mark: "г" },
+    { pushed: false, mark: "ш" },
+    { pushed: false, mark: "щ" },
+    { pushed: false, mark: "з" },
+    { pushed: false, mark: "х" },
+    { pushed: false, mark: "ъ" },
+    { pushed: false, mark: "ф" },
+    { pushed: false, mark: "ы" },
+    { pushed: false, mark: "в" },
+    { pushed: false, mark: "а" },
+    { pushed: false, mark: "п" },
+    { pushed: false, mark: "р" },
+    { pushed: false, mark: "о" },
+    { pushed: false, mark: "л" },
+    { pushed: false, mark: "д" },
+    { pushed: false, mark: "ж" },
+    { pushed: false, mark: "э" },
+    { pushed: false, mark: "я" },
+    { pushed: false, mark: "ч" },
+    { pushed: false, mark: "с" },
+    { pushed: false, mark: "м" },
+    { pushed: false, mark: "и" },
+    { pushed: false, mark: "т" },
+    { pushed: false, mark: "ь" },
+    { pushed: false, mark: "б" },
+    { pushed: false, mark: "ю" },
+    { pushed: false, mark: "ё" },
+];
+
+  
 }
